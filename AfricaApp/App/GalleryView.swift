6884195cf7d06332261162f4f32314564620caf8
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-19 01:26:51 +0530

    Added dynamic grid layout and a slider to change the number of images displayed on UI dynamicalled based on slider

--- AfricaApp/App/GalleryView.swift
+++ AfricaApp/App/GalleryView.swift
@@ -12,9 +12,16 @@ struct GalleryView: View {
     @State private var selectedAnimal: String = "lion"
     
     let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
     
-    // 2. efficient grid definition
-    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    // 3. Dynamic grid layout
+    
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Double = 3.0
+    
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: Int(gridColumn))
+    }
     
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
@@ -26,6 +33,12 @@ struct GalleryView: View {
                     .clipShape(Circle())
                     .overlay(Circle().stroke(Color.white, lineWidth: 8))
                 
+                Slider(value: $gridColumn, in: 2...4, step: 1)
+                    .padding(.horizontal)
+                    .onChange(of: gridColumn, perform: { value in
+                        gridSwitch()
+                    })
+                
                 LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
                     ForEach(animals) { item in
                         Image(item.image)
@@ -35,9 +48,14 @@ struct GalleryView: View {
                             .overlay(Circle().stroke(Color.white, lineWidth: 1))
                             .onTapGesture {
                                 selectedAnimal = item.image
+                                haptics.impactOccurred()
                             }
                     }
                 }
+                .animation(.easeIn)
+                .onAppear(perform: {
+                    gridSwitch()
+                })
             }
             .padding(.horizontal, 10)
             .padding(.vertical, 50)
