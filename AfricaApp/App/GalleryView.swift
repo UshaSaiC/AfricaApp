c3ea3ace80729e5f34b23895c275a7ead786daac
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-19 00:48:01 +0530

    Added a simple grid view which shows all images

--- AfricaApp/App/GalleryView.swift
+++ AfricaApp/App/GalleryView.swift
@@ -8,9 +8,29 @@
 import SwiftUI
 
 struct GalleryView: View {
+    
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    
+    // Grid id made of 3 things : Data, Grid items array and lazyvgrid or lazyhgrid that brings data and layout together
+    // 1. Simple Grid
+     let gridLayout: [GridItem] = [
+         GridItem(.flexible()),
+         GridItem(.flexible()),
+         GridItem(.flexible())
+     ]
+    
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            Text("Gallery")
+            
+            LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                ForEach(animals) { item in
+                    Image(item.image)
+                        .resizable()
+                        .scaledToFit()
+                        .clipShape(Circle())
+                        .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                }
+            }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
