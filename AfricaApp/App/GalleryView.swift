38d2c4b921631d0486f6a31efcda16d3a795bac9
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-19 00:57:18 +0530

    Added another grid definition that displays images based on the image tapped

--- AfricaApp/App/GalleryView.swift
+++ AfricaApp/App/GalleryView.swift
@@ -9,28 +9,38 @@ import SwiftUI
 
 struct GalleryView: View {
     
+    @State private var selectedAnimal: String = "lion"
+    
     let animals: [Animal] = Bundle.main.decode("animals.json")
     
-    // Grid id made of 3 things : Data, Grid items array and lazyvgrid or lazyhgrid that brings data and layout together
-    // 1. Simple Grid
-     let gridLayout: [GridItem] = [
-         GridItem(.flexible()),
-         GridItem(.flexible()),
-         GridItem(.flexible())
-     ]
+    // 2. efficient grid definition
+    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
     
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
             
-            LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
-                ForEach(animals) { item in
-                    Image(item.image)
-                        .resizable()
-                        .scaledToFit()
-                        .clipShape(Circle())
-                        .overlay(Circle().stroke(Color.white, lineWidth: 1))
+            VStack(alignment: .center, spacing: 30) {
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+                
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                            }
+                    }
                 }
             }
+            .padding(.horizontal, 10)
+            .padding(.vertical, 50)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
