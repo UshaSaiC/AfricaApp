f57bf608084f717f420fc1f472ceea02659aeb1e
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-19 03:12:42 +0530

    Added gri and list view button and a sample page is displayed on selecting grid view option

--- AfricaApp/App/ContentView.swift
+++ AfricaApp/App/ContentView.swift
@@ -9,23 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
+    
+    @State private var isGridViewActive: Bool = false
+    
     var body: some View {
         
-        let animals: [Animal] = Bundle.main.decode("animals.json")
-        
         NavigationView {
-            List {
-                CoverImageView()
-                    .frame(height: 300)
-                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                
-                ForEach(animals) { animal in
-                    NavigationLink(destination: AnimalDetailView(animal: animal)) {
-                        AnimalListItemView(animal: animal)
+            Group{
+                if !isGridViewActive {
+                    List {
+                        CoverImageView()
+                            .frame(height: 300)
+                            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                        
+                        ForEach(animals) { animal in
+                            NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                                AnimalListItemView(animal: animal)
+                            }
+                        }
                     }
+                } else {
+                    Text("Grid view is active")
                 }
             }
             .navigationBarTitle("Africa", displayMode: .large)
+            .toolbar{
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    HStack(spacing: 16) {
+                        Button(action: {
+                            print("List View")
+                            isGridViewActive = false
+                            haptics.impactOccurred()
+                        }){
+                            Image(systemName: "square.fill.text.grid.1x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .primary : .accentColor)
+                        }
+                        Button(action: {
+                            print("Grid View")
+                            isGridViewActive = true
+                            haptics.impactOccurred()
+                        }){
+                            Image(systemName: "square.grid.2x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .accentColor : .primary)
+                        }
+                    }
+                }
+            }
         }
     }
 }
