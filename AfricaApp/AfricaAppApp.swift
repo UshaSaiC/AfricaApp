e34df3f7b53ecdc8627a3f188856d5c2a6a3f206
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-18 16:21:47 +0530

    Initial Commit

---
+++ AfricaApp/AfricaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaAppApp.swift
+//  AfricaApp
+//
+//  Created by Usha Sai Chintha on 18/09/22.
+//
+
+import SwiftUI
+
+@main
+struct AfricaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
